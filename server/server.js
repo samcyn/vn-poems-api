701a0abe0a2f07429a2832739647b1118a65acd8
samcyn <samsoniyanda@outlook.com>
2018-03-28 22:54:40 +0100

    Added GET /poems/:poemId endpoint

--- server/server.js
+++ server/server.js
@@ -23,15 +23,15 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.post('/poems', (req, res) => {
-  var poem = new Poem({
+  var newPoem = new Poem({
     title: req.body.title,
     message: req.body.message,
     location: req.body.location,
     author: new ObjectID()
   });
 
-  poem.save().then((doc) => {
-    res.send(doc);
+  newPoem.save().then((poem) => {
+    res.send({poem});
   }, (e) => {
     res.status(400).send(e);
   });
@@ -45,6 +45,26 @@ app.get('/poems', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+
+app.get('/poems/:poemId', (req, res) => {
+  var id = req.params.poemId;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send()
+  }
+  Poem.findOne({
+    _id: id
+  }).then((poem) => {
+    if(!poem){
+      return res.status(404).send()
+    }
+    res.send({poem});
+  }).catch((e) => {
+    res.status(400).send(e)
+  });
+
+});
+
 
 
 app.listen(port, () => {
